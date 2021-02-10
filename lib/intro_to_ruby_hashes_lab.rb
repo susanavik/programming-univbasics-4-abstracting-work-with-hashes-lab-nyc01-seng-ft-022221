71fd0df51ec16c5f0d153c284a2a88022a0926b3
Susana vik <susana.m.vik@gmail.com>
2021-02-10 02:59:32 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,11 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash_to_read = {
+    location: "New York"
+    occupation: "Bankers"
+  }
+  read_from_hash(hash_to_read, :location)
 end
 
 def update_counting_hash(hash, key)
